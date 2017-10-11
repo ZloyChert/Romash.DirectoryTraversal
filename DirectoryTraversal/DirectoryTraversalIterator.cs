88963d57fa29358a852c264f0850bd85707c4ca9
zloychert <19860714@mail.ru>
2017-10-11 20:16:05 +0300

    Basic functional added

---
+++ DirectoryTraversal/DirectoryTraversalIterator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryTraversal
+{
+    public class DirectoryTraversalIterator
+    {
+        public Action<string> ActionOnFile { get; set; }
+        public Func<string, bool> Checker { get; set; }
+
+        public DirectoryTraversalIterator(Action<string> actionOnFile, Func<string, bool> checker)
+        {
+            ActionOnFile = actionOnFile;
+            Checker = checker;
+        }
+
+        public void WalkDirectoryTree(DirectoryInfo root)
+        {
+            try
+            {
+                var files = root.GetFiles("*.*");
+                var subDirs = root.GetDirectories();
+
+                foreach (FileInfo fi in files)
+                {
+                    if (Checker(fi.FullName))
+                    {
+                        ActionOnFile(fi.FullName);
+                    }
+                }
+
+                foreach (DirectoryInfo dirInfo in subDirs)
+                {
+                    if (Checker(dirInfo.FullName))
+                    {
+                        ActionOnFile(dirInfo.FullName);
+                        WalkDirectoryTree(dirInfo);
+                    }
+                }
+            }
+            catch (UnauthorizedAccessException e)
+            {
+            }
+        }
+    }
+}
---
+++ SimpleTester/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DirectoryTraversal;
+
+namespace SimpleTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] drives = Environment.GetLogicalDrives();
+
+            System.IO.DriveInfo di = new System.IO.DriveInfo(drives[1]);
+
+            System.IO.DirectoryInfo rootDir = di.RootDirectory;
+            
+            DirectoryTraversalIterator dt = new DirectoryTraversalIterator(Console.WriteLine, s => true);
+            dt.WalkDirectoryTree(rootDir);
+            Console.Read();
+        }
+    }
+}
